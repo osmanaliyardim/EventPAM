136e1b838e75b95f01a21cfe68e0284a5e03e4b6
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-05 13:16:21 +0300

    FEAT: add DDD models

---
+++ src/BuildingBlocks/BuildingBlocks/Core/Model/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿using EventPAM.BuildingBlocks.Core.Event;
+
+namespace EventPAM.BuildingBlocks.Core.Model;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+    IEvent[] ClearDomainEvents();
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Core/Model/IVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace EventPAM.BuildingBlocks.Core.Model;
+
+// To handle optimistic concurrency
+public interface IVersion
+{
+    long Version { get; set; }
+}
