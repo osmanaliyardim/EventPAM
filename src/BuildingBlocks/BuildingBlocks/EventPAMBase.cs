e6f5f0c99a85d2d32cba984af53dc2bb2cc1a299
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-05 09:12:21 +0300

    FEAT: Create base class to handle global configs

---
+++ src/BuildingBlocks/BuildingBlocks/EventPAMBase.cs
@@ -0,0 +1,32 @@
+﻿namespace EventPAM.BuildingBlocks;
+
+public class EventPAMBase
+{
+    public class Messages
+    {
+        // Error Messages
+
+
+        // Warning Messages
+
+
+        // Info Messages
+
+
+        // Errors with variables
+
+
+    }
+
+    public class Configs
+    {
+        // Connection Strings - Keys - Secrets
+
+    }
+
+    public class Endpoints
+    {
+        // Endpoints - Urls
+
+    }
+}
