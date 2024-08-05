9bfc74284b37a096a07af84893834cfef79a6c96
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-05 12:36:58 +0300

    FEAT: add validation behavior

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/Validation/ValidationTool.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace EventPAM.BuildingBlocks.Behaviors.Validation;
+
+public class ValidationTool
+{
+    public static void Validate(IValidator validator, object entity)
+    {
+        ValidationContext<object> context = new(entity);
+
+        ValidationResult result = validator.Validate(context);
+
+        if (!result.IsValid)
+            throw new ValidationException(result.Errors);
+    }
+}
