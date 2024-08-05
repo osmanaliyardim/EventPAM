9d861d153624f91223b4ee056ab13079875cc217
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-05 13:17:20 +0300

    FEAT: add dynamic filter and sorting mechanism

---
+++ src/BuildingBlocks/BuildingBlocks/Core/Persistance/Dynamic/Filter.cs
@@ -0,0 +1,27 @@
+﻿namespace EventPAM.BuildingBlocks.Core.Persistence.Dynamic;
+
+public class Filter
+{
+    public string Field { get; set; } = default!;
+
+    public string Operator { get; set; } = default!;
+
+    public string? Value { get; set; }
+
+    public string? Logic { get; set; }
+
+    public IEnumerable<Filter>? Filters { get; set; }
+
+    public Filter() { }
+
+    public Filter
+        (string field, string @operator, string? value, string? logic, IEnumerable<Filter>? filters)
+        : this()
+    {
+        Field = field;
+        Operator = @operator;
+        Value = value;
+        Logic = logic;
+        Filters = filters;
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Core/Persistance/Dynamic/Sort.cs
@@ -0,0 +1,15 @@
+﻿namespace EventPAM.BuildingBlocks.Core.Persistence.Dynamic;
+
+public class Sort
+{
+    public string Field { get; set; } = default!;
+    public string Dir { get; set; } = default!;
+
+    public Sort() { }
+
+    public Sort(string field, string dir)
+    {
+        Field = field;
+        Dir = dir;
+    }
+}
