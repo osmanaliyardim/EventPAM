c78f16b0023c2f435320da7ee9f6e0c8f1c85d05
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-06 10:14:36 +0300

    FEAT: add web extensions, helpers and configs

---
+++ src/BuildingBlocks/BuildingBlocks/Web/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Asp.Versioning;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventPAM.BuildingBlocks.Web;
+
+[Route(Configs.BASE_API_PATH)]
+[ApiController]
+[ApiVersion("1.0")]
+public abstract class BaseController : ControllerBase
+{
+    private IMapper _mapper = default!;
+    private IMediator _mediator = default!;
+
+    protected IMediator Mediator =>
+        _mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
+
+    protected IMapper Mapper => _mapper ??= HttpContext.RequestServices.GetService<IMapper>()!;
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Web/EndpointConfig.cs
@@ -0,0 +1,10 @@
+﻿using Asp.Versioning.Builder;
+
+namespace EventPAM.BuildingBlocks.Web;
+
+public class EndpointConfig
+{
+    public const string BaseApiPath = Configs.BASE_API_PATH;
+
+    public static ApiVersionSet VersionSet { get; private set; } = default!;
+}
