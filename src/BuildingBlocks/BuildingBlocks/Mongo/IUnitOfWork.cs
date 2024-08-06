9ee66f4ab40cc03f59597e54f6a4360a329a48e8
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-06 10:48:39 +0300

    FEAT: add mongodb context and repository base

---
+++ src/BuildingBlocks/BuildingBlocks/Mongo/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+namespace EventPAM.BuildingBlocks.Mongo;
+
+public interface IUnitOfWork : IDisposable
+{
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task CommitAsync(CancellationToken cancellationToken = default);
+}
+
+public interface IUnitOfWork<out TContext> : IUnitOfWork
+    where TContext : class
+{
+    TContext Context { get; }
+}
