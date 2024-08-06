9c9e3febb292fa5abec0c11310f61869d54c6bae
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-06 10:11:59 +0300

    FEAT: add EntityFramework Core extensions and configs

---
+++ src/BuildingBlocks/BuildingBlocks/EFCore/Extensions.cs
@@ -0,0 +1,121 @@
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ardalis.GuardClauses;
+using Humanizer;
+using Microsoft.EntityFrameworkCore.Metadata;
+using EventPAM.BuildingBlocks.Web;
+using EventPAM.BuildingBlocks.Core.Model;
+
+namespace EventPAM.BuildingBlocks.EFCore;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCustomDbContext<TContext>(
+        this IServiceCollection services)
+        where TContext : DbContext, IDbContext
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        services.AddValidateOptions<PostgresOptions>();
+
+        services.AddDbContext<TContext>((sp, options) =>
+        {
+            var postgresOptions = sp.GetRequiredService<PostgresOptions>();
+
+            Guard.Against.Null(options, nameof(postgresOptions));
+
+            options.UseNpgsql(postgresOptions?.ConnectionString,
+                    dbOptions =>
+                    {
+                        dbOptions.MigrationsAssembly(typeof(TContext).Assembly.GetName().Name);
+                    })
+                .UseSnakeCaseNamingConvention();
+        });
+
+        services.AddScoped<IDbContext>(provider => provider.GetService<TContext>()!);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseMigration<TContext>(this IApplicationBuilder app, IWebHostEnvironment env)
+        where TContext : DbContext, IDbContext
+    {
+        MigrateDatabaseAsync<TContext>(app.ApplicationServices).GetAwaiter().GetResult();
+
+        if (!env.IsEnvironment("test"))
+        {
+            SeedDataAsync(app.ApplicationServices).GetAwaiter().GetResult();
+        }
+
+        return app;
+    }
+
+    public static void FilterSoftDeletedProperties(this ModelBuilder modelBuilder)
+    {
+        Expression<Func<IAggregate, bool>> filterExpr = e => !e.IsDeleted;
+        foreach (var mutableEntityType in modelBuilder.Model.GetEntityTypes()
+                     .Where(m => m.ClrType.IsAssignableTo(typeof(IEntity))))
+        {
+            // modify expression to handle correct child type
+            var parameter = Expression.Parameter(mutableEntityType.ClrType);
+            var body = ReplacingExpressionVisitor
+                .Replace(filterExpr.Parameters.First(), parameter, filterExpr.Body);
+            var lambdaExpression = Expression.Lambda(body, parameter);
+
+            // set filter
+            mutableEntityType.SetQueryFilter(lambdaExpression);
+        }
+    }
+
+    public static void ToSnakeCaseTables(this ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            // Replace table names
+            entity.SetTableName(entity.GetTableName()?.Underscore());
+
+            var tableObjectIdentifier =
+                StoreObjectIdentifier.Table(entity.GetTableName()?.Underscore()!, entity.GetSchema());
+
+            // Replace column names
+            foreach (var property in entity.GetProperties())
+            {
+                property.SetColumnName(property.GetColumnName(tableObjectIdentifier)?.Underscore());
+            }
+
+            foreach (var key in entity.GetKeys())
+            {
+                key.SetName(key.GetName()?.Underscore());
+            }
+
+            foreach (var key in entity.GetForeignKeys())
+            {
+                key.SetConstraintName(key.GetConstraintName()?.Underscore());
+            }
+        }
+    }
+
+    private static async Task MigrateDatabaseAsync<TContext>(IServiceProvider serviceProvider)
+        where TContext : DbContext, IDbContext
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<TContext>();
+        await context.Database.MigrateAsync();
+    }
+
+    private static async Task SeedDataAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var seeders = scope.ServiceProvider.GetServices<IDataSeeder>();
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAllAsync();
+        }
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks/EFCore/IDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using EventPAM.BuildingBlocks.Core.Event;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EventPAM.BuildingBlocks.EFCore;
+
+public interface IDbContext
+{
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+    IReadOnlyList<IDomainEvent> GetDomainEvents();
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    Task BeginTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
+
+    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
+
+    IExecutionStrategy CreateExecutionStrategy();
+
+    Task ExecuteTransactionalAsync(CancellationToken cancellationToken = default);
+}
