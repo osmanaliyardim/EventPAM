f0edbed9ef73c1fac04fe822bba8512dfd2c9521
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-08-06 10:15:20 +0300

    FEAT: add utilities and base config and messages

--- src/BuildingBlocks/BuildingBlocks/EventPAMBase.cs
+++ src/BuildingBlocks/BuildingBlocks/EventPAMBase.cs
@@ -9,14 +9,25 @@ public class EventPAMBase
         public const string VALIDATION = "Validation failed: ";
         public const string NOT_AUTHENTICATED = "You are not authenticated.";
         public const string NOT_AUTHORIZED = "You are not authorized.";
+        public const string CANNOT_CREATE_DB = "Cannot create table if there are pending migrations.";
+        public const string IVERSION_ERROR = "Try to find IVersion";
+        public const string IAGGREGATE_ERROR = "Try to find IAggregate";
+        public const string POLLY_SERVICE_DOWN = "Service shutdown during {BreakDuration} after {RetryCount} failed retries";
+        public const string POLLY_FAILED = "Request failed with {StatusCode}. Waiting {TimeSpan} before next retry. Retry attempt {RetryCount}.";
 
         // Warning Messages
         public const string PERFORMANCE_WARNING = "Performance -> ";
+        public const string DB_CONSISTENCY_WARNING = "The record no longer exists in the database, The record has been deleted by another user.";
 
         // Info Messages
         public const string CACHE_FETCHED = "Fetched from Cache -> ";
         public const string CACHE_ADDED = "Added to Cache -> ";
         public const string CACHE_REMOVED = "Removed Cache -> ";
+        public const string SERVICE_RESTARTED = "Service restarted";
+        public const string PMBS_STARTED = "PersistMessage Background Service Start";
+        public const string PMBS_STOPPED = "PersistMessage Background Service Stop";
+        public const string PMBS_MESSAGE_SAVED = "Message with id: {MessageID} and delivery type: {DeliveryType} saved in persistence message store.";
+        public const string PMBS_MESSAGE_PROCESSED = "InternalCommand with id: {EventID} and delivery type: {DeliveryType} processed from the persistence message store.";
     }
 
     public static class Configs
@@ -31,6 +42,11 @@ public class EventPAMBase
         public const string SERILOG_GRAYLOG = "SeriLogConfigurations:GraylogConfiguration";
         public const string SERILOG_FILE = "SeriLogConfigurations:FileLogConfiguration";
         public const string SERILOG_ELASTICSEARCH = "SeriLogConfigurations:ElasticSearchConfiguration";
+        public const string POLLY_GRPC_CB_LOGGER = "PollyGrpcCircuitBreakerPoliciesLogger";
+        public const string POLLY_HTTP_CB_LOGGER = "PollyHttpClientCircuitBreakerPoliciesLogger";
+        public const string ENVIRONMENT_VARIABLE = "ASPNETCORE_ENVIRONMENT";
+        public const string DEFAULT_CONN_STR = "DefaultConnection";
+        public const string BASE_API_PATH = "api/v{version:apiVersion}";
     }
 
     public static class Endpoints
---
+++ src/BuildingBlocks/BuildingBlocks/Utils/ServiceLocator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EventPAM.BuildingBlocks.Utils;
+
+public class ServiceLocator
+{
+    private IServiceProvider _currentServiceProvider;
+    private static IServiceProvider _serviceProvider = default!;
+
+    public ServiceLocator(IServiceProvider currentServiceProvider)
+    {
+        _currentServiceProvider = currentServiceProvider;
+    }
+
+    public static ServiceLocator Current
+    {
+        get
+        {
+            return new ServiceLocator(_serviceProvider);
+        }
+    }
+
+    public static void SetLocatorProvider(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public object GetInstance(Type serviceType)
+    {
+        return _currentServiceProvider.GetService(serviceType)!;
+    }
+
+    public TService GetInstance<TService>()
+    {
+        return _currentServiceProvider.GetService<TService>()!;
+    }
+}
